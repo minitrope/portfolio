0d333fd87962c19615f996ee5110f812e077eee2
Romain Gauthier <romain.gauthier@monkeypatch.me>
2025-12-15 16:21:17 +0100

    Add the compiled /docs.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("assets");
+    return {
+        dir: {
+            output: "docs"
+        },
+        clean: true
+    };
 };
